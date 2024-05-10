0c8ee65497021c87b3936d069b0300d209bc6a8f
EC2 Default User <ec2-user@ip-10-0-33-149.ec2.internal>
2024-05-10 15:59:35 +0000

    workshop code

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,55 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { WorkshopPipelineStage } from "./pipeline-stage";
+
+export class WorkshopPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Creates a CodeCommit repository called 'WorkshopRepo'
+    const repo = new Repository(this, "WorkshopRepo", {
+      repositoryName: "WorkshopRepo",
+    });
+
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.codeCommit(repo, "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+
+    const deploy = new WorkshopPipelineStage(this, "Deploy");
+    const deployStage = pipeline.addStage(deploy);
+
+    // Add Below
+
+    deployStage.addPost(
+        new CodeBuildStep('TestViewerEndpoint', {
+          projectName: 'TestViewerEndpoint',
+          envFromCfnOutputs: {
+            ENDPOINT_URL:  deploy.hcViewerUrl
+          },
+          commands: [
+            'curl -Ssf $ENDPOINT_URL'
+          ]
+        }),
+  
+        new CodeBuildStep('TestAPIGatewayEndpoint', {
+          projectName: 'TestAPIGatewayEndpoint',
+          envFromCfnOutputs: {
+            ENDPOINT_URL: deploy.hcEndpoint
+          },
+          commands: [
+            'curl -Ssf $ENDPOINT_URL',
+            'curl -Ssf $ENDPOINT_URL/hello',
+            'curl -Ssf $ENDPOINT_URL/test'
+          ]
+        })
+    );
+  }
+}
